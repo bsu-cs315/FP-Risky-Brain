13a152a6315e6bf42b7e410e24f086ae02c707f9
adamgack <apgack@gmail.com>
2020-10-24 01:37:27 -0400

    Add minor tweaks

--- project/src/Bullet.gd
+++ project/src/Bullet.gd
@@ -24,3 +24,9 @@ func _on_Bullet_area_entered(area):
 	if owner.has_method("take_damage"):
 		owner.take_damage(damage, area, shooter)
 		queue_free()
+	
+
+
+func _on_Bullet_body_entered(body):
+	if body.name == "Walls":
+		queue_free()
