95ac064121a3b14580de55b0cf5d7fe0d7b0b67d
kade-d <kldentel@bsu.edu>
2020-11-06 01:14:27 -0500

    Fix Main.tscn parsing error from merge

--- project-client/src/EnemySpawner.gd
+++ project-client/src/EnemySpawner.gd
@@ -11,7 +11,7 @@ onready var zombie: Resource = load("res://src/enemies/Zombie.tscn")
 func _ready():
 	rng.randomize()
 	add_child(spawn_timer)
-	spawn_timer.connect("timeout", self, "spawn_enemy")
+	var _err_func_timer_timeout = spawn_timer.connect("timeout", self, "spawn_enemy")
 	spawn_timer.one_shot = true
 	spawn_timer.start(1)
 
@@ -19,7 +19,7 @@ func _ready():
 func spawn_enemy():
 	var random_spawn_point_index: int = rng.randi_range(0, spawn_points.size() - 1)
 	var random_spawn_point: Node2D = spawn_points[random_spawn_point_index]
-	var random_spawn_timer_cooldown: float = rng.randf_range(0.1, 3)
+	var random_spawn_timer_cooldown: float = rng.randf_range(0.5, 3)
 	var new_enemy: Node2D = zombie.instance()
 	get_parent().add_child(new_enemy)
 	new_enemy.position = random_spawn_point.position
