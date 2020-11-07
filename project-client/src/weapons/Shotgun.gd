8961faa49a31c3f14033cb4b5c7aff799aff1720
kade-d <kldentel@bsu.edu>
2020-11-06 20:04:57 -0500

    Add individual shotgun bullet relaoding

--- project-client/src/weapons/Shotgun.gd
+++ project-client/src/weapons/Shotgun.gd
@@ -1,8 +1,12 @@
 extends Weapon
 
 
+const BULLET_RELOAD_TIME = .4
+
 var pellet_count:= 10
 var pellet_inaccuracy:= deg2rad(20.0)
+var reloading:= false
+var reload_timer := Timer.new()
 
 
 func _init(player: Node) -> void:
@@ -23,13 +27,12 @@ func _init(player: Node) -> void:
 	player_sprite.connect("animation_finished", self, "stop_animation")
 	audio_player = player.get_node("WeaponAudioPlayer")
 	audio_player.stream = load("res://assets/audio/shotgun_fire.wav")
+	shot_cooldown_timer.one_shot = true
+	shooter.call_deferred("add_child", shot_cooldown_timer)
 
 
 func shoot() -> void:
-	if !shot_cooldown_timer.is_inside_tree():
-		shooter.add_child(shot_cooldown_timer)
-		shot_cooldown_timer.one_shot = true
-	if shot_cooldown_timer.time_left == 0.0 and ammo_mag_current > 0:
+	if shot_cooldown_timer.time_left == 0.0 and ammo_mag_current > 0 and not reloading:
 		for i in range(floor(-pellet_count / 2.0), floor(pellet_count / 2.0)): #evenly spreads pellets
 			var pellet_angle: float = shooter.shoot_dir.angle() + (i * pellet_inaccuracy) / pellet_count
 			var proj: Area2D = bullet.instance()
@@ -39,12 +42,35 @@ func shoot() -> void:
 			shooter.get_node("/root/World").add_child(proj)
 		shot_cooldown_timer.start(shot_cooldown)
 		audio_player.play()
-		player_sprite.frame = 1
-		player_sprite.play()
+		player_sprite.frame = 1 # play shooting from the second frame
+		player_sprite.play() # animation set in player.gd change_weapon_sprite
 		decrement_ammo(1)
-		
-		
+
+
 func stop_animation() -> void:
 	player_sprite.stop()
 
 
+func reload() -> void:
+	if !reloading:
+		reloading = true
+		reload_timer.one_shot = true
+		reload_timer.connect("timeout", self, "load_single_bullet")
+		shooter.add_child(reload_timer)
+		# start timer so it can add a bullet when it expires
+		reload_timer.start(BULLET_RELOAD_TIME)
+
+
+func load_single_bullet() -> void:
+	if get_empty_mag_slots() > 0:
+		ammo_mag_current += 1
+	if get_empty_mag_slots() > 0:
+		# if there are still empty spots in mag, add another bullet when the timer expires again
+		reload_timer.start(BULLET_RELOAD_TIME)
+	else:
+		reloading = false
+		shooter.remove_child(reload_timer)
+
+
+func get_empty_mag_slots() -> int:
+	return int(min(ammo_mag_max, ammo_total_current) - ammo_mag_current)
